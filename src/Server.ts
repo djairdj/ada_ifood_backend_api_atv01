d5cad03fa33333ff71872b7a8b6cb2a04c923565
Djair Alves <djair.dj@gmail.com>
2024-01-21 10:06:45 -0300

    Remoção do código referente a outro tipo de conexão

--- src/Server.ts
+++ src/Server.ts
@@ -1,12 +1,11 @@
 import express, { Request, Response } from "express";
 
-import { connection } from "./base/Connection";
-
 import { v4 as uuidV4 } from 'uuid';
 
 import fs from 'fs';
 
 const path = './src/base/db.json';
+
 function readJsonFile(file_path: string) {
   try {
     const conteudoArquivo = fs.readFileSync(file_path, 'utf8');
@@ -29,8 +28,7 @@ export const server = async (port: number = 3003) => {
   // Obter todos os médicos
   app.get('/',
     async (_, resp: Response) => {
-      // const doctorsList = await getAllDoctors();
-      const doctorsList = await JgetAllDoctors();
+      const doctorsList = await getAllDoctorsInJSON();
       return resp.status(200).json(doctorsList);
     });
   // Obter 1 médico pelo cpf
@@ -38,8 +36,7 @@ export const server = async (port: number = 3003) => {
     async (req, resp: Response) => {
       if (!req.params.cpf)
         return resp.status(404).json([]);
-      // const doctor = await getDoctorByCPF(req.params.cpf);
-      const doctor = await JgetDoctorByCPF(req.params.cpf);
+      const doctor = await getDoctorByCpfInJSON(req.params.cpf);
       if (doctor)
         resp.status(200).json(doctor);
       else
@@ -49,8 +46,7 @@ export const server = async (port: number = 3003) => {
   app.get('/crm/:crm?',
     async (req, resp: Response) => {
       if (!req.params.crm) return resp.status(404).json([]);
-      // const doctor = await getDoctorByCRM(req.params.crm);
-      const doctor = await JgetDoctorByCRM(req.params.crm);
+      const doctor = await getDoctorByCrmInJSON(req.params.crm);
       if (doctor) resp.status(200).json(doctor);
       else resp.status(404).end();
     });
@@ -58,8 +54,7 @@ export const server = async (port: number = 3003) => {
   app.get('/name/:name?',
     async (req, resp: Response) => {
       if (!req.params.name) return resp.status(404).end();
-      // const doctor = await getDoctorsByName(req.params.name);
-      const doctor = await JgetDoctorsByName(req.params.name);
+      const doctor = await getDoctorsByNameInJSON(req.params.name);
       if (doctor) resp.status(200).json(doctor);
       else resp.status(404).end();
     });
@@ -67,8 +62,7 @@ export const server = async (port: number = 3003) => {
   app.get('/id/:id?',
     async (request: Request, resp: Response) => {
       if (!request.params.id) return resp.status(404).json([]);
-      // const doctor = await getDoctorById(request.params.id);
-      const doctor = await JgetDoctorById(request.params.id);
+      const doctor = await getDoctorByIdInJSON(request.params.id);
       if (doctor) resp.status(200).json(doctor);
       else resp.status(404).end();
     });
@@ -76,16 +70,14 @@ export const server = async (port: number = 3003) => {
   app.post('/',
     async (request, response: Response) => {
       request.body['id'] = uuidV4();
-      // const doctor = await addDoctor(request.body);
-      const doctor = await JaddDoctor(request.body);
+      const doctor = await addDoctorInJSON(request.body);
       if (doctor) response.status(201).json(doctor);
       else response.status(403).json({});
     });
   // Atualizar 1 médico
   app.put('/',
     async (request, response: Response) => {
-      // const doctor = await updateDoctorById(request.body);
-      const doctor = await JupdateDoctorById(request.body);
+      const doctor = await updateDoctorByIdInJSON(request.body);
       if (doctor) response.status(201).json(doctor);
       else response.status(403).json({});
     });
@@ -93,115 +85,13 @@ export const server = async (port: number = 3003) => {
   app.delete('/:id?',
     async (request, response) => {
       if (!request.params.id) return response.status(404).json([]);
-      // const doctor = await deleteDoctorById(request.params.id);
-      const doctor = await JdeleteDoctorById(request.params.id);
+      const doctor = await deleteDoctorByIdInJSON(request.params.id);
       if (doctor) response.status(200).json(doctor);
       else response.status(404).end();
     });
 }
 
-
-async function addDoctor(doctor: any) {
-  let query: any, x: any;
-  try {
-    [query] = await connection.execute(
-      `INSERT INTO tb_doctor values(
-          '${doctor.id}', '${doctor.name}', '${doctor.cpf}', '${doctor.crm}',
-          '${doctor.title}', '${doctor.description}');`
-    );
-    x = await getDoctorByCPF(doctor.cpf);
-    return x;
-  } catch (error: any) {
-    console.error(error.message);
-  }
-}
-async function getDoctorByCPF(cpf: string) {
-  try {
-    let [doctor]: any = await connection.execute(
-      `SELECT * FROM tb_doctor WHERE cpf = '${cpf}';`
-    );
-    return doctor[0];
-  } catch (error: any) {
-    console.error("ERRO: " + error.message);
-  }
-}
-async function getDoctorByCRM(crm: string) {
-  try {
-    let [doctor]: any = await connection.execute(
-      `SELECT * FROM tb_doctor WHERE crm = '${crm}';`
-    );
-    return doctor[0];
-  } catch (error: any) {
-    console.error("ERRO: " + error.message);
-  }
-}
-async function getDoctorById(id: string) {
-  try {
-    let [doctor]: any = await connection.execute(
-      `SELECT * FROM tb_doctor WHERE id = '${id}';`
-    );
-    return doctor[0];
-  } catch (error: any) {
-    console.error("ERRO: " + error.message);
-  }
-}
-async function getDoctorsByName(name: string) {
-  try {
-    const [doctorList] = await connection.execute(
-      `SELECT * FROM tb_doctor WHERE name like CONCAT('%', '${name}', '%') order by name;`
-    );
-    return doctorList;
-  } catch (error: any) {
-    console.error("ERRO: " + error.message);
-  }
-}
-async function getAllDoctors() {
-  try {
-    const [doctorList] = await connection.execute(
-      `SELECT * FROM tb_doctor order by name;`
-    );
-    return doctorList;
-  } catch (error: any) {
-    console.error("ERRO: " + error.message);
-  }
-}
-async function updateDoctorById(body: any) {
-  let id = body.id, result: any;
-  if (id) {
-    try {
-      [result] = await connection.execute(
-        `UPDATE tb_doctor SET
-        name = '${body.name}',
-        cpf = '${body.cpf}',
-        crm = '${body.crm}',
-        title = '${body.title}',
-        description = '${body.description}'
-        WHERE id = '${id}'
-        ;`
-      );
-      if (result['affectedRows'] > 0)
-        result = await getDoctorById(id);
-    } catch (error: any) {
-      console.error(error.message);
-    }
-  } else console.log("Não foi possível atualizar o médico sem o id.");
-  return result;
-}
-async function deleteDoctorById(id: string) {
-  try {
-    const [doctor, result]: any = [
-      await getDoctorById(id),
-      await connection.execute(`DELETE FROM tb_doctor WHERE id = '${id}';`)
-    ];
-    if (doctor && result[0].affectedRows > 0)
-      return doctor;
-  } catch (error: any) {
-    console.error("ERRO: " + error.message);
-  }
-}
-
-
-async function JaddDoctor(doctor: any) {
+async function addDoctorInJSON(doctor: any) {
   try {
     let result: any = readJsonFile(path);
     let exist = result.some((i: any) => (
@@ -214,25 +104,25 @@ async function JaddDoctor(doctor: any) {
     console.error(error.message);
   }
 }
-async function JgetDoctorByCPF(cpf: string) {
+async function getDoctorByCpfInJSON(cpf: string) {
   let result = readJsonFile(path);
   result = result.filter((i: any) => i.cpf == cpf);
   if (result.length == 0) return undefined;
   return result[0];
 }
-async function JgetDoctorByCRM(crm: string) {
+async function getDoctorByCrmInJSON(crm: string) {
   let result = readJsonFile(path);
   result = result.filter((i: any) => i.crm == crm);
   if (result.length == 0) return undefined;
   return result[0];
 }
-async function JgetDoctorById(id: string) {
+async function getDoctorByIdInJSON(id: string) {
   let result = readJsonFile(path);
   result = result.filter((i: any) => i.id == id);
   if (result.length == 0) return undefined;
   return result[0];
 }
-async function JgetDoctorsByName(name: string) {
+async function getDoctorsByNameInJSON(name: string) {
   let result = readJsonFile(path);
   result = result.filter((i: any) => {
     let nome = i.name.toLowerCase();
@@ -240,7 +130,7 @@ async function JgetDoctorsByName(name: string) {
   });
   return result;
 }
-async function JgetAllDoctors() {
+async function getAllDoctorsInJSON() {
   try {
     let result = readJsonFile(path);
     return result;
@@ -248,7 +138,7 @@ async function JgetAllDoctors() {
     console.error("ERRO: " + error.message);
   }
 }
-async function JupdateDoctorById(body: any) {
+async function updateDoctorByIdInJSON(body: any) {
   let id = body.id, result: any = undefined;
   if (id) {
     try {
@@ -266,7 +156,7 @@ async function JupdateDoctorById(body: any) {
   } else console.log("Não foi possível atualizar o médico sem o id.");
   return result;
 }
-async function JdeleteDoctorById(id: string) {
+async function deleteDoctorByIdInJSON(id: string) {
   let result: any = undefined;
   if (id) {
     try {
